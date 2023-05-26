de67a1c8796bbb071ae596d698d3a99e00b27b35
pedroleni <pedroleridanieto@hotmail.com>
2023-05-26 21:38:45 +0200

    cambios fallo mongoDB

--- src/api/manga/manga.model.js
+++ src/api/manga/manga.model.js
@@ -1,18 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const schema = new Schema(
+  {
+    image: { type: String, unique: true, required: true },
+    name: { type: String, unique: true, required: true },
+    description: { type: String, unique: true, required: true },
+  },
 
-{
-    images: {type: String, unique: true, required: true},
-    name: {type: String, unique: true, required: true},
-    description: {type: String, unique: true, required: true},
-
-},
-
-{
-    timestamps: true
-}
+  {
+    timestamps: true,
+  }
 );
 
-module.exports = mongoose.model('manga', schema);
+module.exports = mongoose.model("manga", schema);
